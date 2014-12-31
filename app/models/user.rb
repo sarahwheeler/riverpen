627c9639803acf5529aff94d969f5eed93eb0e35
Sarah Wheeler <sarahcwheeler@gmail.com>
2014-12-31 00:28:50 -0600

    Updates user model for either username or email address in recovering  password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,13 +20,13 @@ class User < ActiveRecord::Base
   attr_accessor :login
 
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      where(conditions).first
-    end
-  end
+	def self.find_first_by_auth_conditions(warden_conditions)
+	  conditions = warden_conditions.dup
+	  if login = conditions.delete(:login)
+	    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+	  else
+	    where(conditions).first
+	  end
+	end
 
 end
