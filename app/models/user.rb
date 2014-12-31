809d2bb5b393c36ab2a714aa86598a466a1c27d8
Sarah Wheeler <sarahcwheeler@gmail.com>
2014-12-31 00:03:23 -0600

    Changes Devise setup to have username login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,24 @@ class User < ActiveRecord::Base
 
   has_many :streams
 
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, 
+  					:uniqueness => {
+					    :case_sensitive => false
+					  }
+
+  # For Devise: Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
 end
