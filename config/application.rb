eb51fa8b02343ad13f727526dd1060bff073aa69
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-01-02 16:19:11 -0600

    Fixes glyphicons routing issue.

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Riverpen
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.paths << "#{Rails}/assets/fonts"
+    config.assets.paths << "#{Rails}/assets/stylesheets/fonts"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
