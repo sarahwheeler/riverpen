65daf7e09176d2ea46b0f1e6f0d09842d8772840
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-01-15 20:55:12 -0600

    Adds stream name to Streamline page, automatic capitalization for stream categories.

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,13 +1,21 @@
 class Stream < ActiveRecord::Base
-	
+
+	before_save :capitalize_category
+
 	belongs_to :user
 
 	# OWNERSHIP
 	has_many :posts
 	has_many :subcategories
 
+	# VALIDATIONS
+
 	validates :user_id, presence: true
 	validates :category, presence: true
 	validates :public, presence: true
+
+	def capitalize_category
+  	self.category.capitalize!
+	end
 
 end
