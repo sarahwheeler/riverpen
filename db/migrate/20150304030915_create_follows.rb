25f3d9a233605f2dd611e040a380d62825922403
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-03-03 21:10:42 -0600

    Creates basic Follows db.

---
+++ db/migrate/20150304030915_create_follows.rb
@@ -0,0 +1,11 @@
+class CreateFollows < ActiveRecord::Migration
+  def change
+    create_table :follows do |t|
+      t.integer :user_id
+      t.integer :stream_id
+      t.integer :stream_user_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150118055718) do
+ActiveRecord::Schema.define(version: 20150304030915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150118055718) do
     t.text     "content"
     t.integer  "user_id"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "follows", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "stream_id"
+    t.integer  "stream_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
