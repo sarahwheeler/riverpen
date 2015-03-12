d8118a85897231949c97ffb165133313e16d7c9a
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-03-11 22:32:12 -0500

    Refactoring main streamline helper.

--- app/helpers/streamline_helper.rb
+++ app/helpers/streamline_helper.rb
@@ -2,19 +2,23 @@ module StreamlineHelper
 
 	def streamed_list
 		# refactor into model methods
-		@post_ids = []
+		@all_posts = []
 		if @user.follows.any?
 			@user.follows.each do |f|
 				post_collection = []
 				stream = Stream.find(f.stream_id)
-				stream.posts.order('created_at').each do |p| do 					
-					post_collection << p.id 
+				stream.posts.order('created_at').each do |p| 					
+					post_collection << p
 				end
-				@post_ids << post_collection
+				@all_posts << post_collection
 			end
 		end
+		@streamline = @all_posts.flatten  # incomplete
+
+
+		
 	end
 
-	# take @post_ids, sort and display them
+
 
 end
