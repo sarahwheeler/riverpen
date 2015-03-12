d880b916927088a32d3580f43e8e6e9525802845
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-03-11 22:12:26 -0500

    Starts streamline helper work.

--- app/helpers/streamline_helper.rb
+++ app/helpers/streamline_helper.rb
@@ -1,2 +1,20 @@
 module StreamlineHelper
+
+	def streamed_list
+		# refactor into model methods
+		@post_ids = []
+		if @user.follows.any?
+			@user.follows.each do |f|
+				post_collection = []
+				stream = Stream.find(f.stream_id)
+				stream.posts.order('created_at').each do |p| do 					
+					post_collection << p.id 
+				end
+				@post_ids << post_collection
+			end
+		end
+	end
+
+	# take @post_ids, sort and display them
+
 end
