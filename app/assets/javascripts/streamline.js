5c63a62b379e3c44154f84377cae390fe076276e
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-03-12 22:51:07 -0500

    Work on streamline page and its tabs.

--- app/assets/javascripts/streamline.js
+++ app/assets/javascripts/streamline.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('#me a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$('#following a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+$('#followers a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
