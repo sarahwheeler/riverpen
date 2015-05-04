59d7f5e4894a94b138b8ebf16ef738c4bff09862
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-03 21:31:16 -0500

    Starts setting up more Minitests.

---
+++ test/unit/blog_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+class TestBlog < Minitest::Test
+
+end
---
+++ test/unit/post_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+require 'minitest/autorun'
+
+class TestPost < Minitest::Test
+
+end
